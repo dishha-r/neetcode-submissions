0dca3f658b57eb7fe69da38af868dde144f1751d
dishha-r <dishha23@gmail.com>
2026-09-09 13:39:29 +0530

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int xorr1=0, xorr2=0;
+        for(int i=0; i<n; i++){
+            xorr1 = xorr1 ^ nums[i];
+            xorr2 = xorr2 ^ i;
+        }
+        xorr2 = xorr2 ^ n;
+        return xorr1 ^ xorr2;
+    }
+}
